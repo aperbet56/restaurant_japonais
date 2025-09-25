7b8644864ce52caab5b5a298b517fe3b986e005c
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-09-25 17:40:09 +0200

    Ajout de la fonction getCurrentYear

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ Puis j'enlève cette classe en JavaScript lorsque je suis sûre que le site a bi
 window.addEventListener("load", () => {
   document.body.classList.remove("loading__animation");
 });
+
+// Récupération des éléments HTML5
+const footerCopyrightYear = document.querySelector(".footer__text__year");
+
+// Déclaration de la fonction getCurrentYear qui va permettre l'affichage dynamique de l'année
+const getCurrentYear = () => {
+  // Récupération de la date actuelle stockée dans la constante date
+  const date = new Date();
+  //console.log(date);
+
+  // Récupération de l'année stockée dans la constante year
+  const year = date.getFullYear();
+  //console.log(year);
+
+  // Affichage dynamique de l'année en cours
+  footerCopyrightYear.textContent = `${year}`;
+};
+// Appel de la fonction getCurrentYear()
+getCurrentYear();
